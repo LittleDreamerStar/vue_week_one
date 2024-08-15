ddd4ab4e6555f95a761d5aa467553714cdafcf31
Natto Tsai <fracture_star@hotmail.com>
2024-08-16 00:36:05 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue_week_one/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
